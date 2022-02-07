6d611cc77a931046b99dc200009f56fed30a6f51
bearishtrader <bearish_trader@yahoo.com>
2022-02-06 20:08:49 -0600

    Deployment changes

--- ReimbursementsApp/src/main/resources/frontend/globals/globals.js
+++ ReimbursementsApp/src/main/resources/frontend/globals/globals.js
@@ -1,5 +1,5 @@
-//let domain = "http://34.221.46.159:9000";
-let domain = "http://localhost:9000";
+let domain = "http://34.221.46.159:9000";
+//let domain = "http://localhost:9000";
 const currency = "$";
 const reimbType = ['LODGING', 'TRAVEL', 'FOOD', 'OTHER'];
 const reimbStatus = ['Pending','Approved','Denied'];
